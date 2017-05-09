047e0af9af98f5fc3e6f8a7e9c7fbac3096fc4a8
olegsvs <oleg.texet@gmail.com>
2017-05-09 04:11:13 +0300

    add MACaddress-like editText auto-columns and checkFormat

--- app/src/main/java/ru/olegsvs/mtkmacgen/MainPage.java
+++ app/src/main/java/ru/olegsvs/mtkmacgen/MainPage.java
@@ -1,10 +1,14 @@
 package ru.olegsvs.mtkmacgen;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -13,12 +17,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.security.auth.login.LoginException;
 
 
 public class MainPage extends AppCompatActivity {
 
     public static String TAG = "olegsvs_macgen";
     public Context context;
+    public EditText mMacEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +32,15 @@ public class MainPage extends AppCompatActivity {
         setContentView(R.layout.activity_main_page);
 
         context = getApplicationContext();
+        mMacEdit = (EditText) findViewById(R.id.editMACaddress);
+        registerAfterMacTextChangedCallback();
     }
 
     public void testFunc(View view) throws IOException, InterruptedException {
         SupportChecker sc = new SupportChecker();
         sc.checkDeviceSupport();
         Log.i("device support : ", "testFunc: " + sc.isSupported);
-        Log.i("extract assets ", "testFunc: " + extract_dd_binary());
-        for (int i = 0; i < 5; i++) {
-            MacTools.randomMACAddress();
-        }
+        Log.i("extract assets : ", "testFunc: " + extract_dd_binary());
     }
 
     public boolean extract_dd_binary() {
@@ -64,5 +69,136 @@ public class MainPage extends AppCompatActivity {
         File testFile = new File(this.getFilesDir() + "/dd");
         Log.i(MainPage.TAG, "extract_dd_binary: " + testFile.exists());
         return testFile.exists();
+    }
+
+    /**
+     * Registers TextWatcher for MAC EditText field. Automatically adds colons,
+     * switches the MAC to upper case and handles the cursor position.
+     */
+    private void registerAfterMacTextChangedCallback() {
+        mMacEdit.addTextChangedListener(new TextWatcher() {
+            String mPreviousMac = null;
+
+            /* (non-Javadoc)
+             * Does nothing.
+             * @see android.text.TextWatcher#afterTextChanged(android.text.Editable)
+             */
+            @Override
+            public void afterTextChanged(Editable arg0) {
+            }
+
+            /* (non-Javadoc)
+             * Does nothing.
+             * @see android.text.TextWatcher#beforeTextChanged(java.lang.CharSequence, int, int, int)
+             */
+            @Override
+            public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+            }
+
+            /* (non-Javadoc)
+             * Formats the MAC address and handles the cursor position.
+             * @see android.text.TextWatcher#onTextChanged(java.lang.CharSequence, int, int, int)
+             */
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String enteredMac = mMacEdit.getText().toString().toUpperCase();
+                String cleanMac = clearNonMacCharacters(enteredMac);
+                String formattedMac = formatMacAddress(cleanMac);
+
+                int selectionStart = mMacEdit.getSelectionStart();
+                formattedMac = handleColonDeletion(enteredMac, formattedMac, selectionStart);
+                int lengthDiff = formattedMac.length() - enteredMac.length();
+
+                setMacEdit(cleanMac, formattedMac, selectionStart, lengthDiff);
+            }
+
+            /**
+             * Strips all characters from a string except A-F and 0-9.
+             * @param mac       User input string.
+             * @return          String containing MAC-allowed characters.
+             */
+            private String clearNonMacCharacters(String mac) {
+                return mac.toString().replaceAll("[^A-Fa-f0-9]", "");
+            }
+
+            /**
+             * Adds a colon character to an unformatted MAC address after
+             * every second character (strips full MAC trailing colon)
+             * @param cleanMac      Unformatted MAC address.
+             * @return              Properly formatted MAC address.
+             */
+            private String formatMacAddress(String cleanMac) {
+                int grouppedCharacters = 0;
+                String formattedMac = "";
+
+                for (int i = 0; i < cleanMac.length(); ++i) {
+                    formattedMac += cleanMac.charAt(i);
+                    ++grouppedCharacters;
+
+                    if (grouppedCharacters == 2) {
+                        formattedMac += ":";
+                        grouppedCharacters = 0;
+                    }
+                }
+
+                // Removes trailing colon for complete MAC address
+                if (cleanMac.length() == 12)
+                    formattedMac = formattedMac.substring(0, formattedMac.length() - 1);
+
+                return formattedMac;
+            }
+
+            /**
+             * Upon users colon deletion, deletes MAC character preceding deleted colon as well.
+             * @param enteredMac            User input MAC.
+             * @param formattedMac          Formatted MAC address.
+             * @param selectionStart        MAC EditText field cursor position.
+             * @return                      Formatted MAC address.
+             */
+            private String handleColonDeletion(String enteredMac, String formattedMac, int selectionStart) {
+                if (mPreviousMac != null && mPreviousMac.length() > 1) {
+                    int previousColonCount = colonCount(mPreviousMac);
+                    int currentColonCount = colonCount(enteredMac);
+
+                    if (currentColonCount < previousColonCount) {
+                        formattedMac = formattedMac.substring(0, selectionStart - 1) + formattedMac.substring(selectionStart);
+                        String cleanMac = clearNonMacCharacters(formattedMac);
+                        formattedMac = formatMacAddress(cleanMac);
+                    }
+                }
+                return formattedMac;
+            }
+
+            /**
+             * Gets MAC address current colon count.
+             * @param formattedMac      Formatted MAC address.
+             * @return                  Current number of colons in MAC address.
+             */
+            private int colonCount(String formattedMac) {
+                return formattedMac.replaceAll("[^:]", "").length();
+            }
+
+            /**
+             * Removes TextChange listener, sets MAC EditText field value,
+             * sets new cursor position and re-initiates the listener.
+             * @param cleanMac          Clean MAC address.
+             * @param formattedMac      Formatted MAC address.
+             * @param selectionStart    MAC EditText field cursor position.
+             * @param lengthDiff        Formatted/Entered MAC number of characters difference.
+             */
+            private void setMacEdit(String cleanMac, String formattedMac, int selectionStart, int lengthDiff) {
+                mMacEdit.removeTextChangedListener(this);
+                if (cleanMac.length() <= 12) {
+                    mMacEdit.setText(formattedMac);
+                    mMacEdit.setSelection(selectionStart + lengthDiff);
+                    mPreviousMac = formattedMac;
+                } else {
+                    mMacEdit.setText(mPreviousMac);
+                    mMacEdit.setSelection(mPreviousMac.length());
+                }
+                mMacEdit.addTextChangedListener(this);
+            }
+        });
     }
 }
